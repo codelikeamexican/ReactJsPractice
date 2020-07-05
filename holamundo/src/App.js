971c0e9d5a1deeec6b42a7661f85116c43a4b86e
codelikeamexican <codelikeamexican@gmail.com>
2020-07-04 21:46:06 -0700

    crear componente presentacional

--- holamundo/src/App.js
+++ holamundo/src/App.js
@@ -3,11 +3,13 @@ import logo from './logo.svg';
 import './App.css';
 import Stateful from './components/Stateful'
 import Stateless from './components/Stateless'
+import Presentacional from './components/Presentational'
 function App() {
   return (
     <div className="App">
      <Stateful/>
      <Stateless/>
+     <Presentacional/>
     </div>
   );
 }
