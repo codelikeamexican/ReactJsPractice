d18dc0b7e24a515424f0b330c8106378a24cb821
codelikeamexican <codelikeamexican@gmail.com>
2020-07-06 06:45:57 -0700

    validations to show (or not) code blocks

--- holamundo/src/components/HolaMundo.jsx
+++ holamundo/src/components/HolaMundo.jsx
@@ -2,10 +2,14 @@ import React from 'react'
 
 const HolaMundo = ()=>{
     const hello ='holiwi';
+    const isTrue = false;
     return (
         <div className="HolaMundo">
+            
             <h1>{hello}</h1>
             <h2>Curso Especial de React</h2>
+            {isTrue ? <h1>Esto es verdadero</h1> : <h5>Soy Falso</h5>}
+            {isTrue && <h4>Soy verdadero</h4>}
         </div>
     );
 };
